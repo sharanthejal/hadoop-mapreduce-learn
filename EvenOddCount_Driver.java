dcd156b993e9094380582c2e6a6361dc03a1e4ea
sharan <sharanthejal@users.noreply.github.com>
2020-01-22 19:26:15 +0530

    Create EvenOddCount_Driver.java

---
+++ EvenOddCount_Driver.java
@@ -0,0 +1,55 @@
+package com.hadoop.learn.mr.evenodd;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class EvenOddCount_Driver {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		
+		Configuration conf= new  Configuration();
+		String[] files=new GenericOptionsParser(conf,args).getRemainingArgs();
+		Path input=new Path(files[0]);
+		Path output=new Path(files[1]);
+		//Create Job instance
+		Job job=Job.getInstance(conf);
+		job.setJarByClass(EvenOddCount_Driver.class);
+		job.setJobName("EvenOddCount_MR");
+		
+		//Set MapOutputKeyClass and Value class and also reducers
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		
+		//Set Mapper and Reducer classes
+		job.setMapperClass(EvenOddCount_Mapper.class);
+		job.setReducerClass(EvenOddCount_Reducer.class);
+		
+		//set InputFormat class and OutputFormatClass
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		//Set InputPath and OutputPath for getting and putting data from and to HDFS
+		FileInputFormat.addInputPath(job, input);
+		FileOutputFormat.setOutputPath(job, output);
+		
+		//start Job and exit 
+		//Submit the job to the cluster and wait for it to finish.
+		System.exit(job.waitForCompletion(true)?0:1);
+		
+		
+	}
+
+}
